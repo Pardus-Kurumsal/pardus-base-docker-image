f081f1d677283a091d976461544f22f0620a5af9
Cihangir Akturk <cihangir@pardus.org.tr>
2017-02-20 15:05:13 +0300

    Initial implementation of pardus docker image builder
    Signed-off-by: Cihangir Akturk <cihangir@pardus.org.tr>

---
+++ create-base-pardus-image.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Cihangir Akturk
+# In order for this script to run properly, you need to make
+# sure that you have pardus-archive-keyring installed on your
+# system.
+
+set -e
+
+if ! [ $(id -u) = 0 ]; then
+    echo "You need superuser privileges to run this script!"
+    exit 127
+fi
+
+PARDUS_REPO_URL=http://161.9.194.187:33333/pardus
+SUITE="pardus-devel"
+TARGET=$(mktemp -d "pardus.base.XXXXXX")
+
+# clean up the mess on premature exits
+clean_up() {
+    rm -fr "${TARGET}"
+}
+trap clean_up EXIT INT
+
+debootstrap "${SUITE}" "${TARGET}" "${PARDUS_REPO_URL}"
+
+# Clean up chroot
+chroot "${TARGET}" apt-get clean
+rm -rf "${TARGET}/var/lib/apt/lists/*"
+
+tar -C "${TARGET}" -c . | docker import - pardus-base
